55033cd6644c718e79fa4e7ccf0eaae39de74a10
Black_Moss <kytheminecraft@outlook.com>
2026-06-21 01:36:58 +0800

    Initial commit: Bark mod

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Bark;
+
+[BepInPlugin(Guid, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string Guid = "org.cucnmc.bark";
+    public const string Name = "Bark";
+    public const string Version = "1.0.0";
+    internal new static ManualLogSource Logger;
+    private readonly Harmony _harmony = new(Guid);
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+        _harmony.PatchAll();
+
+        Logger.LogInfo("Bark loaded!");
+    }
+}
